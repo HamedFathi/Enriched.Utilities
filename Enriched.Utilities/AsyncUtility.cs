650e85c122faa3d94dd2d401c89243b9c3c6ecd7
hamedfathi <hamedfathi@outlook.com>
2022-04-18 20:59:19 +0200

    chore(Utilities): AsyncUtility added.

---
+++ Enriched.Utilities/AsyncUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Enriched.Utilities
+{
+    public class AsyncUtility
+    {
+        private static readonly TaskFactory MyTaskFactory = new
+            TaskFactory(CancellationToken.None,
+                TaskCreationOptions.None,
+                TaskContinuationOptions.None,
+                TaskScheduler.Default);
+
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            return AsyncUtility.MyTaskFactory
+                .StartNew<Task<TResult>>(func)
+                .Unwrap<TResult>()
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        public static void RunSync(Func<Task> func)
+        {
+            AsyncUtility.MyTaskFactory
+                .StartNew<Task>(func)
+                .Unwrap()
+                .GetAwaiter()
+                .GetResult();
+        }
+    }
+}
